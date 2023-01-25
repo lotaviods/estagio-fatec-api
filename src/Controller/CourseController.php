016e047b8f8129832d37c23f64cba892d9c62e05
lotaviods <l.otavio9099@gmail.com>
2023-01-25 03:10:10 -0300

    more changes

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Course;
+use App\Helper\ResponseHelper;
 use App\Repository\CourseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -22,6 +24,34 @@ class CourseController extends AbstractController
         $repository = $entityManager->getRepository(Course::class);
 
         $repository->findAll();
+
+        $coursesResult = $repository->findAll();
+        $courseArray = [];
+
+        foreach ($coursesResult as $course) {
+            $courseArray[] = $course->toArray();
+        }
+
+        return new JsonResponse($courseArray, Response::HTTP_OK, [], false);;
+    }
+
+    #[Route('/api/course', name: 'create_couse')]
+    public function createCourse(ManagerRegistry $doctrine, Request $request)
+    {
+        $name = $request->get("name");
+
+        if($name == null) return ResponseHelper::missingParameterResponse("name");
+
+        $entityManager = $doctrine->getManager();
+
+        $repository = $entityManager->getRepository(Course::class);
+
+        $course = new Course();
+
+        $course->setName($name);
+
+        /** @var CourseRepository $repository */
+        $repository->save();
 
         $coursesResult = $repository->findAll();
         $courseArray = [];
