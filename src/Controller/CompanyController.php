eadcf407faf1ebf9c76821abdb500045f1b0f009
lotaviods <l.otavio9099@gmail.com>
2023-05-15 14:53:25 -0300

    feat: Get company by id

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Company;
+use App\Helper\ProfilePictureHelper;
 use App\Helper\ResponseHelper;
 use App\Repository\CompanyRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -14,6 +15,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CompanyController extends AbstractController
 {
+    private ProfilePictureHelper $profilePictureHelper;
+
+    public function __construct(ProfilePictureHelper $profilePictureHelper)
+    {
+        $this->profilePictureHelper = $profilePictureHelper;
+    }
+
     #[Route('/api/v1/company', name: 'company_v1', methods: ['GET'])]
     public function getAllCompanies(ManagerRegistry $doctrine): JsonResponse
     {
@@ -35,14 +43,35 @@ class CompanyController extends AbstractController
         return new JsonResponse($companiesArray, Response::HTTP_OK, [], false);
     }
 
+    #[Route('/api/v1/company/{id}', name: 'company_by_id_v1', methods: ['GET'])]
+    public function getCompanyById(ManagerRegistry $doctrine, Request $request): JsonResponse
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $id = $request->get("id");
+
+        if ($id == null) return ResponseHelper::missingParameterResponse("id");
+
+        $entityManager = $doctrine->getManager();
+        /** @var CompanyRepository $repository */
+
+        $repository = $entityManager->getRepository(Company::class);
+        $company = $repository->find($id);
+
+        $array = $company->toArray();
+        $array["profile_picture"] = $this->profilePictureHelper->getFullProfileUrl($company->getProfilePicture());
+
+        return new JsonResponse($array, Response::HTTP_OK, [], false);
+    }
+
     #[Route('/api/v1/company', name: 'delete_company_v1', methods: ['DELETE'])]
     public function deleteCompany(ManagerRegistry $doctrine, Request $request): JsonResponse
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
-        $id  = $request->get("id");
+        $id = $request->get("id");
 
-        if($id == null) return ResponseHelper::missingParameterResponse("id");
+        if ($id == null) return ResponseHelper::missingParameterResponse("id");
 
         $entityManager = $doctrine->getManager();
         /** @var CompanyRepository $repository */
