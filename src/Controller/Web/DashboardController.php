5cad63eaef8eaa5d75536eb90751688b5e6e1e7f
lotaviods <l.otavio9099@gmail.com>
2023-05-23 01:08:47 -0300

    feat: Add count fields in dashboard route

--- src/Controller/Web/DashboardController.php
+++ src/Controller/Web/DashboardController.php
@@ -2,20 +2,25 @@
 
 namespace App\Controller\Web;
 
+use App\Service\DashboardService;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardController extends AbstractController
 {
     #[Route('/api/v1/web/dashboard', name: 'dashboard', methods: ['GET'])]
-    public function dashboard(Request         $request,
-                              ManagerRegistry $doctrine
+    #[IsGranted("IS_AUTHENTICATED_FULLY")]
+    public function dashboard(Request          $request,
+                              ManagerRegistry  $doctrine,
+                              DashboardService $dashboardService
     ): JsonResponse
     {
+        $user = $this->getUser();
 
-        return new JsonResponse([]);
+        return new JsonResponse($dashboardService->getDashboardInfo($user));
     }
 }
---
+++ src/Service/DashboardService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Service;
+
+use Doctrine\ORM\NativeQuery;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class DashboardService
+{
+    private ManagerRegistry $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    public function getDashboardInfo(?UserInterface $user): array
+    {
+        $resultArray = [];
+
+        if (in_array('ROLE_ADMIN', $user->getRoles())) {
+            $resultArray["panel_data"] = $this->getEntitiesCount();
+        }
+
+        return $resultArray;
+    }
+
+
+    private function getEntitiesCount(): array
+    {
+        //todo: Make cache of this result
+        $manager = $this->doctrine->getManager();
+        $rsm = new ResultSetMappingBuilder($manager);
+        $rsm->addScalarResult('student_count', 'students', 'integer');
+        $rsm->addScalarResult('course_count', 'courses', 'integer');
+        $rsm->addScalarResult('company_count', 'companies', 'integer');
+        $rsm->addScalarResult('job_offer_count', 'job_offers', 'integer');
+        /** @var NativeQuery $query */
+        $query = $manager->createNativeQuery('
+        SELECT
+            (SELECT COUNT(*) FROM student) AS student_count,
+            (SELECT COUNT(*) FROM course) AS course_count,
+            (SELECT COUNT(*) FROM company) AS company_count,
+            (SELECT COUNT(*) FROM job_offer) AS job_offer_count
+    ', $rsm);
+
+        return $query->getArrayResult();
+    }
+}
