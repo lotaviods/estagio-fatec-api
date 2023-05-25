ed8cba3bb412ea8fd49c6d1228cf7addac636690
lotaviods <l.otavio9099@gmail.com>
2023-05-25 13:21:29 -0300

    const: Update JobExpirience constants

--- src/Constants/JobExperience.php
+++ src/Constants/JobExperience.php
@@ -4,6 +4,7 @@ namespace App\Constants;
 class JobExperience
 {
     const NoExperience = 0;
-    const JuniorExperience = 1;
-    const SeniorExperience = 2;
+    const TraineeExperience = 1;
+    const JuniorExperience = 2;
+    const MaxExperience = 2;
 }
