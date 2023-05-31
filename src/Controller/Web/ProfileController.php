05c7d42d9e0d30082e83d37c18226961465ea52a
lotaviods <l.otavio9099@gmail.com>
2023-05-31 15:15:08 -0300

    feat: Adding profile update end-point

--- src/Controller/Web/ProfileController.php
+++ src/Controller/Web/ProfileController.php
@@ -10,12 +10,14 @@ use App\Repository\AdministratorRepository;
 use App\Service\WebProfileService;
 use AWS\CRT\Log;
 use Doctrine\Persistence\ManagerRegistry;
+use PhpParser\JsonDecoder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use function MongoDB\BSON\fromJSON;
 
 class ProfileController extends AbstractController
 {
@@ -27,5 +29,26 @@ class ProfileController extends AbstractController
         $user = $this->getUser();
 
         return new JsonResponse($webProfileService->getLoginProfileInfo($user), 200);
+    }
+
+    #[Route('/api/v1/web/profile', name: 'web_profile_v1', methods: ['PUT'])]
+    #[IsGranted("IS_AUTHENTICATED_FULLY")]
+    public function updateProfile(Request $request): JsonResponse
+    {
+        $response = null;
+        /** @var Login $user */
+        $user = $this->getUser();
+        $roles = $user->getRoles();
+
+        $parameters = $request->query->all();
+
+        if (in_array('ROLE_COMPANY', $roles)) {
+            $response = $this->forward('App\Controller\CompanyController::updateCompany', $parameters);
+        }
+        if (in_array('ROLE_ADMIN', $roles)) {
+            $response = $this->forward('App\Controller\AdminController::updateAdmin', $parameters);
+        }
+
+        return new JsonResponse((new JsonDecoder)->decode($response->getContent()), 200);
     }
 }
